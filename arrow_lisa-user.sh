eb87369c98e3e987d21e3ed86f9ce4d213517106
TechWiz007 <gajarebhargav@gmail.com>
2022-07-14 22:26:35 +0530

    update some repos and build user

--- arrow_lisa-user.sh
+++ arrow_lisa-user.sh
@@ -25,18 +25,26 @@ echo "Syncing source is done, now time to sync device specific code..."
 rm -rf hardware/qcom-caf/sm8350/display
 rm -rf hardware/google/pixel-sepolicy
 git clone https://github.com/TechWiz007/device_xiaomi_lisa.git -b twelve device/xiaomi/lisa
-git clone https://gitlab.com/lisa-oss/vendor_xiaomi_lisa.git -b twelve vendor/xiaomi/lisa
+git clone https://gitlab.com/PixelOS-Devices/vendor_xiaomi_lisa.git -b twelve vendor/xiaomi/lisa
 git clone https://github.com/Atom-X-Devs/android_kernel_xiaomi_sm7325.git -b inline kernel/xiaomi/lisa
 git clone https://github.com/PixelOS-Devices/hardware_google_pixel-sepolicy.git -b twelve hardware/google/pixel-sepolicy
 git clone https://github.com/PixelOS-Pixelish/hardware_qcom-caf_sm8350_display.git -b twelve hardware/qcom-caf/sm8350/display
-git clone https://gitlab.com/lisa-oss/neutron-clang.git -b Neutron-15 prebuilts/clang/host/linux-x86/clang-neutron
+git clone https://gitlab.com/dakkshesh07/neutron-clang.git -b Neutron-15 prebuilts/clang/host/linux-x86/clang-neutron
+git clone https://gitlab.com/ghostrider-reborn/android_vendor_xiaomi_lisa-firmware.git -b sapphire vendor/xiaomi/lisa-firmware
+
+echo "Time to make some changes in source now"
+
+# Force Neutron Clang in vendor/arrow/config/BoardConfigKernel.mk
+cd vendor/arrow/config
+sed -i 's/r416183b1/neutron/g' BoardConfigKernel.mk
+cd ../../..
 
 echo "Let us start the build for lisa"
 
 # Start the build
 . build/envsetup.sh
-ccache -M 150G
-lunch arrow_lisa-userdebug
+ccache -M 200G
+lunch arrow_lisa-user
 m bacon -j$(nproc --all)
 
 echo "Build Completed Succesfully! Thank you for using this script..."
